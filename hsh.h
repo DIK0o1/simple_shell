30416fb1750a58f502a718bcf644b175491d12a4
Abdelrahman Mohamed Ibrahim <84192043+DIK0o1@users.noreply.github.com>
2023-11-16 15:49:59 +0200

    Add files via upload

---
+++ hsh.h
@@ -0,0 +1,34 @@
+#ifndef _HSH_H_
+#define _HSH_H_
+
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <stdbool.h>
+#include <signal.h>
+#include <stdio.h>
+
+#include "typedef.h"
+#include "dict.h"
+#include "envt.h"
+#include "dets.h"
+#include "string.h"
+#include "getline.h"
+#include "err.h"
+#include "list.h"
+
+extern char **environ;
+
+bool read_dets(dets_t *dets);
+int parse_dets(dets_t *dets);
+void free_tokens(char ***tokens);
+int exec(dets_t *dets);
+int _exec(dets_t *dets);
+void _sigint(int signal);
+char *get_path(dets_t *dets, list_t *path);
+void free_word_arr(char **words_arr);
+
+#endif /* _HSH_H_*/
