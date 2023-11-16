1daaff84cec44ce19961d795c7a62f835f06c50f
DIK0o1 <abd0odeco@gmail.com>
2023-11-16 05:58:18 -0800

    removed

--- _sign.c
+++
@@ -1,7 +0,0 @@
-#include "hsh.h"
-
-void _sigint(int signal __attribute__((unused)))
-{
-	fflush(STDIN_FILENO);
-	write(STDERR_FILENO, "\n$ ", 3);
-}
--- typedef.h
+++
@@ -1,8 +0,0 @@
-#ifndef _TYPEDEF_H_
-#define _TYPEDEF_H_
-
-typedef struct dets dets_t;
-typedef struct dict dict_t;
-typedef struct list_s list_t;
-
-#endif /* _TYPEDEF_H_*/
